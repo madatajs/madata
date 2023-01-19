d30208d38b1249bc3be162cfea3617e91aa194bf
Lea Verou <lea@verou.me>
2023-01-19 18:21:29 +0200

    Fix locations

--- backends/index-fn.js
+++ backends/index-fn.js
@@ -1,9 +1,9 @@
 /**
  * Export all backends without side effects
  */
-export {default as Element} from "./element.js";
-export {default as Local} from "./local.js";
-export {default as Remote} from "./remote.js";
+export {default as Element} from "./basic/element.js";
+export {default as Local} from "./basic/local.js";
+export {default as Remote} from "./basic/remote.js";
 export * from "./github/index.js";
 export {default as Dropbox} from "./dropbox/index.js";
 export * from "./google/index.js";
