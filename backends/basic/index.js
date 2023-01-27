4fc471019327563521c7c5921fcad274de87c4fb
Lea Verou <lea@verou.me>
2023-01-27 11:58:50 -0500

    Add index for basic backends

---
+++ backends/basic/index.js
@@ -0,0 +1,3 @@
+export {default as Local} from "./local.js";
+export {default as Element} from "./element.js";
+export {default as Remote} from "./remote.js";
--- backends/index-fn.js
+++ backends/index-fn.js
@@ -2,9 +2,8 @@
  * @module index-fn
  * Export all backends without side effects
  */
-export {default as Element} from "./basic/element.js";
-export {default as Local} from "./basic/local.js";
-export {default as Remote} from "./basic/remote.js";
+
+export * from "./basic/index.js";
 export * from "./github/index.js";
 export {default as Dropbox} from "./dropbox/index.js";
 export * from "./google/index.js";
