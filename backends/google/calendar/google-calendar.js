b67f69db9e340f8f0e96bd874d872d4fa44d03f2
Lea Verou <lea@verou.me>
2024-01-13 12:47:08 +0200

    Update backends/google/calendar/google-calendar.js
    Co-authored-by: Dmitry Sharabin <dmitrysharabin@gmail.com>

--- backends/google/calendar/google-calendar.js
+++ backends/google/calendar/google-calendar.js
@@ -54,7 +54,7 @@ export default class GoogleCalendar extends Google {
 	static supportedOptions = ["iCalUID", "maxAttendees", "maxResults", "orderBy", "pageToken", "privateExtendedProperty", "q", "sharedExtendedProperty", "showDeleted", "showHiddenInvitations", "singleEvents", "syncToken", "timeMax", "timeMin", "timeZone", "updatedMin"];
 	static apiDomain = "https://www.googleapis.com/calendar/v3/calendars/";
 	static scopes = ["https://www.googleapis.com/auth/calendar.events", "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"];
-	static host = "calendar.google.com"
+	static host = "calendar.google.com";
 
 	/**
 	 * Parse Calendars URLs.
