05043d8f6ca5b2067372a7aabeb23cb9282c9bd1
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-02-02 18:25:04 +0100

    Switch to the JS config file for more versatility

---
+++ typedoc.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import("typedoc").TypeDocOptions} */
+
+module.exports = {
+	name: "Madata API",
+	plugin: [
+		"typedoc-plugin-merge-modules",
+	],
+	mergeModulesRenameDefaults: true,
+	mergeModulesMergeMode: "module",
+	entryPoints: [
+		"src/*.js",
+		"backends/index.js",
+		"formats/index.js",
+	],
+	exclude: [
+		"src/node-shims.js",
+	],
+	excludeReferences: true,
+	navigation: {
+		includeCategories: true,
+		includeFolders: false,
+	},
+	readme: "API.md",
+	out: "api",
+	// "lightHighlightTheme": "solarized-light", // See https://shiki.matsu.io/themes#bundled-themes
+	// "darkHighlightTheme": "solarized-dark",
+	customCss: "api.css",
+};
