0deee084d92d20e95d31acec8b358ed5c78bb011
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-02-27 22:07:09 +0100

    We should use backend static properties

--- src/backend.js
+++ src/backend.js
@@ -290,7 +290,7 @@ export default class Backend extends EventTarget {
 			url: base ? new URL(source, base) : new URL(source)
 		};
 
-		Object.assign(ret, matchURLs(source, this.constructor.urls) ?? matchURLs(source, this.constructor.urlsRecognized));
+		Object.assign(ret, matchURLs(source, this.urls) ?? matchURLs(source, this.urlsRecognized));
 
 		return ret;
 	}
