09f583c2bb06f35ac77e99d5138b64070c882414
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-07-31 12:21:52 +0200

    Add support for uploading files

--- backends/gitlab/index.js
+++ backends/gitlab/index.js
@@ -1,10 +1,11 @@
 import OAuthBackend from "../../src/oauth-backend.js";
+import { readFile } from "../../src/util.js";
 
 /**
  * @category GitLab
  */
 export default class Gitlab extends OAuthBackend {
-	static capabilities = { auth: true, put: true, upload: false };
+	static capabilities = { auth: true, put: true, upload: true };
 	static defaultPermissions = { read: true };
 
 	static fileBased = true;
@@ -26,6 +27,7 @@ export default class Gitlab extends OAuthBackend {
 
 	static phrases = {
 		"updated_file": (name = "file") => "Updated " + name,
+		"uploaded_file": (name = "file") => "Uploaded " + name,
 	};
 
 	static parseURL (source) {
@@ -48,5 +50,31 @@ export default class Gitlab extends OAuthBackend {
 			commit_message: this.constructor.phrase("updated_file", path),
 		};
 		return this.request(fileCall, body, "PUT");
+	}
+
+	async upload (file, path = file.name) {
+		let { id, branch } = this.ref;
+		let fileCall = `projects/${ id }/repository/files/${ encodeURIComponent(path) }`;
+
+		let type = file.type;
+		let isText = type.startsWith("text/") || type.startsWith("application/") && !type.endsWith("pdf");
+		let content;
+		if (isText) {
+			content = await readFile(file, "Text");
+		}
+		else {
+			content = await readFile(file);
+			content = content.slice(5); // remove “data:”
+			type = type.replace("+", "\\+"); // escape “+” in, e.g., “image/svg+xml”
+			content = content.replace(RegExp(`^${type}(;base64)?,`), "");
+		}
+
+		let body = {
+			branch,
+			content,
+			commit_message: this.constructor.phrase("uploaded_file", path),
+			encoding: isText ? "text" : "base64",
+		};
+		return this.request(fileCall, body, "POST");
 	}
 }
