2054e7f41392f1fab577334b358063ff050cce7d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-07-31 10:45:36 +0200

    Add support for put()

--- backends/gitlab/index.js
+++ backends/gitlab/index.js
@@ -4,7 +4,7 @@ import OAuthBackend from "../../src/oauth-backend.js";
  * @category GitLab
  */
 export default class Gitlab extends OAuthBackend {
-	static capabilities = { auth: true, put: false, upload: false };
+	static capabilities = { auth: true, put: true, upload: false };
 	static defaultPermissions = { read: true };
 
 	static fileBased = true;
@@ -26,12 +26,22 @@ export default class Gitlab extends OAuthBackend {
 
 	static parseURL (source) {
 		let ret = super.parseURL(source);
-		ret.id = encodeURIComponent(ret.id);
+		ret.apiCall = `projects/${ encodeURIComponent(ret.id) }/repository/files/${ ret.path }`;
 		return ret;
 	}
 
 	get (ref = this.ref) {
-		let { id, branch, path } = ref;
-		return this.request(`projects/${ id }/repository/files/${ path }/raw?ref=${ branch }`);
+		let { apiCall, branch } = ref;
+		return this.request(`${ apiCall }/raw?ref=${ branch }`);
+	}
+
+	async put (data, {ref = this.ref} = {}) {
+		let { apiCall, path, branch } = ref;
+		let body = {
+			branch,
+			content: await this.stringify(data),
+			commit_message: `Update file ${ path }`,
+		};
+		return this.request(apiCall, body, "PUT");
 	}
 }
